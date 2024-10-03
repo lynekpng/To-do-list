01ba68d91570104e80f55ce6ba91065b248dc785
lynekpng <lynekpng@gmail.com>
2024-10-03 19:06:37 +0200

    tsyled checkboxes

--- React-to-do-list/tailwind.config.js
+++ React-to-do-list/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "./src/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { backgroundImage: {
+      'check': "url('data:image/svg+xml,%3Csvg viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12.5 4.5L6.5 10.5L3.5 7.5' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E')"},},
   },
   plugins: [],
 }
